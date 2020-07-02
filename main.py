5b4dc5218926528f2a07106c40289b615f782a5d
exoushia <45519518+exoushia@users.noreply.github.com>
2020-07-03 02:58:48 +0530

    The main.py is complete to be called

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from train import train_model
+from utils import *
+import torch.nn as nn
+from sklearn.metrics import classification_report
+
 
 class Config(object):
     embed_size = 300
@@ -18,6 +22,7 @@ class Config(object):
     patience=25
     delta=0.00
     batch_size_test = None
+    
 
 
 if __name__ == '__main__':
@@ -30,5 +35,23 @@ if __name__ == '__main__':
 	test_file=''
 	path_to_glove='glove.840B.300d.pkl'
 	path_to_cpt='Expt_results/checkpoints/checkpoint.pt'
+	figname=["Training.png","Validation.png"]
+	preprocess = False
+	smooth = False
+	test_path = path_to_data + '/' + test_file
+	save_result_path="Expt_results/results.csv")
+	target_names=['class 0', 'class 1', 'class 2','class 3'],
+	title = "Test Set"
 
-    train_model(path_to_data, train_file, val_file, test_file, path_to_glove, path_to_cpt, config,preprocess=False)
+    model, avg_train_losses, avg_val_losses, train_losses_plot, val_accuracies_plot, val_losses_plot, epoch_f1, vocab = train_model(path_to_data, train_file, val_file, test_file, path_to_glove, path_to_cpt, config,preprocess)
+	
+	plot_results(train_losses_plot,val_losses_plot,val_accuracies_plot,figname,smooth)
+	
+	test_loss , test_acc , maintaining_F1 = run_test(test_path,preprocess,vocab,model,target='class')
+	
+	#Only for Test rn - we can modify later 
+	print_classification_report(maintaining_F1,title,target_names,save_result_path)
+	
+	
+	
+	

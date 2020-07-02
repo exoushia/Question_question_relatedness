c321fb8f4890f829905e213af54d6e197a43af9b
meher <meherc99@iitkgp.ac.in>
2020-07-03 04:05:06 +0530

    indentation resolved again

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ from sklearn.metrics import classification_report
 
 
 class Config(object):
-    embed_size = 300
-    hidden_layers = 1
-    hidden_size = 128
-    bidirectional = True
-    output_size = 4
-    epochs = 25
-    lr = 0.001
-    ll_hidden_size=50 #Linear layer hidden sizes
-    batch_size = 32
-    # max_sen_len = 20 # Sequence length for RNN
-    dropout_keep = 0.2
-    sample = 1
-    split_ratio =0.4
-    loss_fn = nn.CrossEntropyLoss()
-    patience=25
-    delta=0.001
-    batch_size_test = None
+	embed_size = 300
+	hidden_layers = 1
+	hidden_size = 128
+	bidirectional = True
+	output_size = 4
+	epochs = 25
+	lr = 0.001
+	ll_hidden_size=50 #Linear layer hidden sizes
+	batch_size = 32
+	# max_sen_len = 20 # Sequence length for RNN
+	dropout_keep = 0.2
+	sample = 1
+	split_ratio =0.4
+	loss_fn = nn.CrossEntropyLoss()
+	patience=25
+	delta=0.001
+	batch_size_test = None
 
 
 if __name__ == '__main__':
@@ -42,7 +42,7 @@ if __name__ == '__main__':
 	target_names=['Direct', 'Duplicate', 'Indirect','Isolated']
 	title = "Test Set"
 
- 	model, avg_train_losses, avg_val_losses, train_losses_plot, val_accuracies_plot, val_losses_plot, epoch_f1, vocab = train_model(path_to_data, train_file, val_file, test_file, path_to_glove, path_to_cpt, config, to_preprocess)
+	model, avg_train_losses, avg_val_losses, train_losses_plot, val_accuracies_plot, val_losses_plot, epoch_f1, vocab = train_model(path_to_data, train_file, val_file, test_file, path_to_glove, path_to_cpt, config, to_preprocess)
 	plot_results(train_losses_plot,val_losses_plot,val_accuracies_plot,figname,smooth)
 	
 	test_loss , test_acc , maintaining_F1 = run_test(test_path,preprocess,vocab,model,target='class')

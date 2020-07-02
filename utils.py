435302ea13e5baf7e6af22bf20bcc1dad426a031
exoushia <45519518+exoushia@users.noreply.github.com>
2020-07-02 11:24:52 +0530

    updating with plotting functions

---
+++ utils.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+class plot_results():
+    #Instantiation of class
+    def __init__(self,train_losses_plot,val_losses_plot,val_accuracies_plot,figname=["Training.png","Validation.png"],smooth=False):
+        self.train_losses_plot = train_losses_plot
+        self.val_losses_plot = val_losses_plot
+        self.val_accuracies_plot = val_accuracies_plot
+        self.smooth = smooth
+        self.figname = figname
+
+        flatten = lambda l: [item for sublist in l for item in sublist]
+        if smooth:
+            #If smooth is True, taking the average of every epoch
+            self.train_losses_plot = np.array([np.mean(loss_iter) for loss_iter in list(self.train_losses_plot) ])
+            self.val_losses_plot = np.array([np.mean(loss_iter) for loss_iter in list(self.val_losses_plot) ])
+            self.val_accuracies_plot = np.array([np.mean(loss_iter) for loss_iter in list(self.val_accuracies_plot) ])
+        else :
+            #If smooth is False, flattening out the list to display avg values for every 10 iterations
+            self.train_losses_plot = flatten(self.train_losses_plot)
+            self.val_losses_plot = flatten(self.val_losses_plot)
+            self.val_accuracies_plot = flatten(self.val_accuracies_plot)
+
+    #After instantiating the class, this is the function that needs to be called:    
+    def run(self,figure_sep=True):
+        %matplotlib inline
+        plt.style.use('classic')
+        plt.figure(figsize=(10,8))
+        print("Starting to plot figures.... \n\n")
+        if figure_sep:
+            fig , ax = plt.subplot(2,1,1)
+            ax.plot(self.train_losses_plot)
+            if self.smooth:
+                title_str = 'Average training loss for every Epoch'
+            else:
+                title_str = "Training losses for every 10 iterations"
+            ax.set(ylabel='Training set values',title = title_str )
+            ax.grid()
+            if self.figname is not None:
+                fig.savefig(self.figname[0])
+            plt.show()
+
+
+
+
+            fig , ax = plt.subplot(2,1,2)
+            ax.plot(self.val_losses_plot,color='blue')
+            ax.plot(self.val_accuracies_plot,color='green')
+            ax.legend(['Val losses','Val Accuracies'],loc='best')
+            if self.smooth:
+                title_str = 'Average Validation losses and accuracy for every Epoch'
+            else:
+                title_str = "Validation losses and accuracy for every 10 iterations"
+            ax.set(ylabel='Validation set values',title = title_str )
+            ax.grid()
+            if self.figname is not None:
+                fig.savefig(self.figname[1])
+            plt.show()
+        
+        else :
+            fig , ax = plt.subplot(2,1,2)
+            ax.plot(self.train_losses_plot,color='red')
+            ax.plot(self.val_losses_plot,color='blue')
+            ax.plot(self.val_accuracies_plot,color='green')
+            ax.legend(['Training_loss','Val loss','Val Accuracies'],loc='best')
+            if self.smooth:
+                title_str = 'Average Training loss Validation losses and accuracy for every Epoch'
+            else:
+                title_str = "Training loss Validation loss and accuracy for every 10 iterations"
+            ax.set(title = title_str )
+            ax.grid()
+            if self.figname is not None:
+                fig.savefig(self.figname[0])
+            plt.show()
+        
+        print("All graphs plotted! \n\n")
